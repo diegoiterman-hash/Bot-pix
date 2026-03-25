9b832597af442f77f6a3a4ebd1b2acbd89f362d8
diegoiterman-hash <diegoiterman@gmail.com>
2026-03-24 22:52:17 -0300

    Add painel command with button interaction

--- commands/painel.js
+++ commands/painel.js
@@ -1,1 +1,21 @@
-Commit changes
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+
+module.exports = {
+    name: "painel",
+    description: "Criar painel",
+
+    async execute(interaction) {
+
+        const row = new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+                .setCustomId("comprar")
+                .setLabel("Comprar")
+                .setStyle(ButtonStyle.Success)
+        );
+
+        await interaction.reply({
+            content: "🛒 Clique no botão para comprar",
+            components: [row]
+        });
+    }
+};
