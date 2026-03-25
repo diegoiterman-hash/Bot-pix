09fe57de01f1aebaf4019ef906e39d505449e363
diegoiterman-hash <diegoiterman@gmail.com>
2026-03-24 22:52:58 -0300

    Add registrarpix command to register PIX key

--- commands/registrarpix.js
+++ commands/registrarpix.js
@@ -1,1 +1,15 @@
-Commit changes
+const fs = require("fs");
+
+module.exports = {
+    name: "registrarpix",
+    description: "Registrar chave pix",
+
+    async execute(interaction) {
+
+        const chave = "SUA_CHAVE_PIX_AQUI";
+
+        fs.writeFileSync("./data/pix.json", JSON.stringify({ chave }, null, 2));
+
+        await interaction.reply("PIX registrado!");
+    }
+};
